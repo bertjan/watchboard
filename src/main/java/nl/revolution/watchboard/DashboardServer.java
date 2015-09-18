826b1326f309c45e284d61c887cec41d569db26b
Bert Jan Schrijver <bertjan@revolution.nl>
2015-09-18 09:06:52 +0200

    Determine app version at build time using maven build timestamp.

--- src/main/java/nl/revolution/watchboard/DashboardServer.java
+++ src/main/java/nl/revolution/watchboard/DashboardServer.java
@@ -6,6 +6,10 @@ import org.eclipse.jetty.server.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class DashboardServer {
 
     private static final Logger LOG = LoggerFactory.getLogger(DashboardServer.class);
@@ -14,9 +18,15 @@ public class DashboardServer {
     public static void main(String... args) throws Exception {
         configureLogging();
 
-        // Set backend app version: unique id for each time the app starts
+        // Set backend app version: unique id for each build.
         // (to enable auto refesh in the frontend on app restart/upgrade).
-        appVersion = "WatchBoard-" + System.currentTimeMillis();
+        appVersion = readBuildTimestampFromVersionProperties();
+        if (appVersion == null || appVersion.endsWith("PLACEHOLDER")) {
+            // Workaround for local deployments since the placeholdr is replaced in the maven build process.
+            appVersion = "WatchBoard-" + System.currentTimeMillis();
+        }
+        LOG.debug("Starting {}.", appVersion);
+
         CloudWatchDataSource dataWorker = new CloudWatchDataSource();
         Server webServer = new WebServer().createServer();
 
@@ -50,7 +60,18 @@ public class DashboardServer {
         LoggerContext logConfig = (LoggerContext) LoggerFactory.getILoggerFactory();
         logConfig.getLogger("ROOT").setLevel(Level.INFO);
         logConfig.getLogger("org.eclipse.jetty").setLevel(Level.INFO);
+        logConfig.getLogger("org.eclipse.jetty.server.handler.ContextHandler").setLevel(Level.ERROR);
         logConfig.getLogger("nl.revolution.watchboard").setLevel(Level.DEBUG);
+    }
+
+    private static String readBuildTimestampFromVersionProperties() throws IOException {
+        InputStream propertyStream = DashboardServer.class.getClassLoader().getResourceAsStream("version.properties");
+        if (propertyStream == null) {
+            return null;
+        }
+        Properties properties = new Properties();
+        properties.load(propertyStream);
+        return properties.getProperty("BUILD_TIMESTAMP");
     }
 
 }
