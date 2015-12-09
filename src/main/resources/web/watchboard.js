6bf13d471a2d9ddf902e31fceda630a75ef15c58
Bert Jan Schrijver <bertjan@revolution.nl>
2015-12-09 11:49:52 +0100

    Move JavaScript code out of HTML.

--- src/main/resources/web/watchboard.js
+++ src/main/resources/web/watchboard.js
@@ -1,3 +1,28 @@
+var appVersion;
+var configLastUpdated;
+var lastAppRefresh = new Date().getTime();
+var lastUpdated = 0;
+var numberOfColumns;
+
+function setURLHash() {
+  var imageOrder = $('#imageList').children('li').map(function(){
+    return $(this).find("img").attr('id')
+  }).get();
+  location.hash = 'columns=' + numberOfColumns + "|" + "imageOrder=" + imageOrder;
+}
+
+
+function setColumns(numCols) {
+  numberOfColumns = numCols;
+  setURLHash();
+  refreshPage();
+}
+
+function refreshPage() {
+  location.reload(true);
+  lastAppRefresh = new Date().getMilliseconds();
+}
+
 function endsWith(str, suffix) {
   return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
@@ -40,3 +65,147 @@ function setGraphsToEqualHeight() {
   setEqualHeight($("#images li"));
 }
 
+
+function renderDashboardList() {
+  $.ajax({
+    url: 'api/v1/dashboards',
+    success:function(data) {
+
+      data.dashboards.sort(function(dash1, dash2) {
+        return dash1.id.localeCompare(dash2.id);
+      });
+
+      $("#dashboards").html("<ul>");
+      for (var i = 0; i < data.dashboards.length; i++) {
+        dashboard = data.dashboards[i];
+        dashboardLink = dashboard.id;
+        if (dashboard.defaultNumberOfColumns) {
+          dashboardLink += "#columns=" + dashboard.defaultNumberOfColumns;
+        }
+
+        $("#dashboards").html($("#dashboards").html() +
+          "<li><a href=\""+ dashboardLink + "\">" + dashboard.title + "</a></li>");
+      }
+      $("#dashboards").html($("#dashboards").html() + "</ul>")
+    },
+    error:function(jqXHR, textStatus,errorThrown) {
+      $("#dashboards").text("No dashboards found.");
+    }
+  });
+}
+
+function startGraphUpdateLoop() {
+  var pathname = window.location.pathname;
+  if (endsWith(pathname, '/')) {
+    pathname = pathname.substring(0, pathname.length - 1);
+  }
+  var dashboardId = pathname.substring(pathname.lastIndexOf('/') + 1, pathname.length);
+
+  // Read number of columns from URL parameter.
+  numberOfColumns = getHashParam()["columns"];
+  if (numberOfColumns == undefined) {
+    numberOfColumns = 2;
+  }
+
+  // Render column selection.
+  maxNumberOfColumns = 4;
+  var columnSelectionHTML = "Columns: ";
+  for (col = 1; col <= maxNumberOfColumns; col++) {
+    // Add switch link.
+    if (col != numberOfColumns) {
+      columnSelectionHTML += "<a href=\"#\" onClick=\"setColumns(" + col + "); event.preventDefault();\">" + col + "</a>";
+    } else {
+      columnSelectionHTML += col;
+    }
+
+    if (col != maxNumberOfColumns) {
+      columnSelectionHTML += " | ";
+    }
+  }
+  $("#columnSelection").html(columnSelectionHTML);
+  var imageWidthPercentage = (100 / numberOfColumns) - 1;
+
+  // Initial rendering.
+  $.ajax({
+    url: '../api/v1/status/' + dashboardId,
+    success: function (data) {
+      appVersion = data.appVersion;
+      configLastUpdated = data.configLastUpdated;
+      $("#title").text(data.title);
+
+      // Sort images based on order in url hash.
+      var imageOrder = getHashParam()["imageOrder"];
+      if(imageOrder) {
+        data.images.sort(function (a, b) {
+          return imageOrder.indexOf(a.id) < imageOrder.indexOf(b.id) ? -1 : 1;
+        });
+      }
+
+      for (var i = 0; i < data.images.length; i++) {
+        image = data.images[i];
+        $("#images").html($("#images").html() +
+          "<li class=\"ui-state-default\" style=\"width: " + imageWidthPercentage + "%\"><a href=\"" + image.url + "\" target=\"_blank\">" +
+          "<img style=\"width: 100%\" id=\"" + image.id + "\" " +
+          "data-lastmodified=\"" + image.lastModified + "\" " +
+          "src=\"" + image.filename + "\" " +
+          "title=\"" + 'Last updated: ' + new Date(image.lastModified) + "\" " +
+          ">" +
+          "</a></li>");
+      }
+      $("#images").html("<ul id=\"imageList\">" + $("#images").html() + "</ul>");
+
+      $("#images ul").sortable({
+        deactivate: function( event, ui ) {
+          setURLHash();
+        }
+      });
+
+
+    }
+  });
+
+  // Periodic update.
+  setInterval(function () {
+    $.ajax({
+      url: '../api/v1/status/' + dashboardId,
+      success: function (data) {
+        // (New backend version OR config update)
+        // AND last refresh was over 30 seconds ago (to throttle refreshes).
+        if (((appVersion != data.appVersion) ||  (configLastUpdated != data.configLastUpdated))
+          && (new Date().getTime() - lastAppRefresh > 30000)) {
+          // Force reload of page from server to keep front- and backend in sync.
+          refreshPage();
+        }
+
+        for (var i = 0; i < data.images.length; i++) {
+          image = data.images[i];
+          imageElement = $('img#' + image.id)
+          storedLastModified = imageElement.attr("data-lastmodified");
+          newLastModified = image.lastModified;
+          if (newLastModified != storedLastModified) {
+            // refresh.
+            imageElement.attr('data-lastmodified', image.lastModified);
+            imageElement.attr('src', image.filename + '?' + image.lastModified);
+            imageElement.attr('title', 'Last updated: ' + new Date(image.lastModified));
+          }
+          if (newLastModified > lastUpdated) {
+            lastUpdated = newLastModified;
+            $("#lastUpdated").text(new Date(lastUpdated));
+          }
+        }
+
+        // Make sure all images have the same height.
+        setGraphsToEqualHeight();
+      }
+    });
+    // Scan each second for updated images.
+  }, 1000);
+
+  // Refresh page after resize. Don't to this instantly to prevent flickering.
+  var resizeTimeout;
+  window.onresize = function(){
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(refreshPage, 100);
+  };
+
+}
